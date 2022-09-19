75ace7c6fc769ed659a83679ce9615a3968793ff
Baaadmtfka <82189244+Baaadmtfka@users.noreply.github.com>
2022-09-19 15:50:20 -0400

    third try

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -172,7 +172,7 @@ public class Basics {
     public static int oddSum(int[] arr) {
         int current_sum = 0;
 
-        for (int i = 1; i < arr.length; i++){
+        for (int i = 0; i < arr.length-1; i++){
             current_sum += arr[i];
         }
         /* TODO (Task 5): Complete this method body using a for-loop.
